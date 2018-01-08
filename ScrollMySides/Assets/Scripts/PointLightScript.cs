0e159da16552c591c41c2e4f566bc8371088e326
alexseguin <segualex@gmail.com>
2018-01-08 12:11:11 -0500

    Added functional torches prefabs and normal map for basic tile shading

---
+++ ScrollMySides/Assets/Scripts/PointLightScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointLightScript : MonoBehaviour {
+
+    Light fire;
+    float lightInt;
+    public float minInt = 6F,  maxInt = 7F;
+
+    // Use this for initialization
+    void Start () {
+        fire = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lightInt = Random.Range(minInt, maxInt);
+        fire.intensity = lightInt;
+	}
+}
