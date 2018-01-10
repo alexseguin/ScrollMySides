49dcb831114dac55dcfc8c5fc192fb67ad38b3bc
alexseguin <segualex@gmail.com>
2018-01-10 17:35:48 -0500

    Newest build upload & map changes

---
+++ ScrollMySides/Assets/Scripts/FallDespawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallDespawn : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+        if(other.tag == "player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
